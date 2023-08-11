24acb70f682b9b9ad6a1a2286d03c720dbe0e61b
Bruno Hoffmann <brunohoffmanndev@gmail.com>
2023-08-11 08:56:23 +0100

    feat: finish second exercise

---
+++ exercicio02/index.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/bubble-sort', (req, res) => {
+  const data = [3,4,2,1,3,2,3,2,3]
+  const tamanho = data.length;
+
+  let sorted;
+  do {
+    sorted = true;
+    for (let i = 0; i < tamanho - 1; i += 2) {
+      if (i + 1 < tamanho && data[i] > data[i + 1]) {
+        [data[i], data[i + 1]] = [data[i + 1], data[i]];
+        sorted = false;
+      }
+      if (i + 2 < tamanho && data[i + 1] > data[i + 2]) {
+        [data[i + 1], data[i + 2]] = [data[i + 2], data[i + 1]];
+        sorted = false;
+      }
+    }
+  } while (!sorted);
+
+  return res.json(data);
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server run port:${port}`);
+});
